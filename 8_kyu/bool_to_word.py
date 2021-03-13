6abd43ddb534d45b1c9c0554bec4a0aa99e71214
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-13 23:53:59 +0300

    Convert boolean values to strings 'Yes' or 'No' added

---
+++ 8_kyu/bool_to_word.py
@@ -0,0 +1,13 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+"""
+
+
+def bool_to_word(boolean):
+    return ["No", "Yes"][boolean]
