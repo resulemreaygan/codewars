4172ccb4d5d3292957efd5b3066f3e2ae4914546
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-13 23:24:59 +0300

    is_divisible added

--- 8_kyu/litres.py
+++ 8_kyu/litres.py
@@ -3,6 +3,8 @@ Author: Resul Emre AYGAN
 """
 
 """
+Project Description: Keep Hydrated!
+
 Nathan loves cycling.
 
 Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
