bbd561ae2463b0099b4d7583df9d4b4b9bb1b2bc
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-13 23:43:53 +0300

    Reversed Strings added

---
+++ 8_kyu/reversed_str.py
@@ -0,0 +1,15 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+"""
+
+
+def solution(string):
+    return string[::-1]
