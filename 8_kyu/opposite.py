d620e7954ca81251aa054b0cf8a2572c2489947f
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-13 23:48:15 +0300

    Opposite number added

---
+++ 8_kyu/opposite.py
@@ -0,0 +1,18 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Opposite number
+
+Very simple, given a number, find its opposite.
+
+Examples:
+1: -1
+14: -14
+-34: 34
+"""
+
+
+def opposite(number):
+    return number * -1
