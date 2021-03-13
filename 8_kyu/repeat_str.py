201c11b4c29facacc08fe8cf939388cb6736f810
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-13 23:46:12 +0300

    String repeat added

---
+++ 8_kyu/repeat_str.py
@@ -0,0 +1,16 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: String repeat
+
+Write a function called repeat_str which repeats the given string src exactly count times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+"""
+
+
+def repeat_str(repeat, string):
+    return repeat * string
