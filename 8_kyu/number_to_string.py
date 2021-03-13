528e5ca2d23730812bc62fa3004be551b57442f0
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-13 23:32:59 +0300

    Convert a Number to a String! added

---
+++ 8_kyu/number_to_string.py
@@ -0,0 +1,19 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Convert a Number to a String!
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+    number_to_string(123) /* returns '123' */
+    number_to_string(999) /* returns '999' */
+"""
+
+
+def number_to_string(num):
+    return str(num)
