cb268cf2a54c8362376e70a1a94c303670ed7003
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-14 19:34:24 +0300

    Reversed Words added

---
+++ 8_kyu/reverse_words.py
@@ -0,0 +1,17 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Reversed Words
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+"""
+
+
+def reverse_words(s):
+    return " ".join(s.split(' ')[::-1])
