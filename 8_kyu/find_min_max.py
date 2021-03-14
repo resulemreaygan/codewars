6ec5af849d326743d62006c23148ecdf8e5d5df0
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-14 19:33:08 +0300

    Find Maximum and Minimum Values of a List added

---
+++ 8_kyu/find_min_max.py
@@ -0,0 +1,30 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Find Maximum and Minimum Values of a List
+
+Your task is to make two functions, max and min (maximum and minimum in PHP and Python) 
+that take a(n) array/vector of integers list as input and outputs, respectively, 
+the largest and lowest number in that array/vector.
+
+#Examples
+
+maximum([4,6,2,1,9,63,-134,566]) returns 566
+minimum([-52, 56, 30, 29, -54, 0, -110]) returns -110
+maximum([5]) returns 5
+minimum([42, 54, 65, 87, 0]) returns 0
+
+#Notes
+
+You may consider that there will not be any empty arrays/vectors.
+"""
+
+
+def minimum(arr):
+    return min(arr)
+
+
+def maximum(arr):
+    return max(arr)
