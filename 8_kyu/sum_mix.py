40340b874c23cd06e6c4d252b290414c8a09dd38
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-14 19:34:44 +0300

    Sum Mixed Array added

---
+++ 8_kyu/sum_mix.py
@@ -0,0 +1,15 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+"""
+
+
+def sum_mix(arr):
+    return sum(map(int, arr))
