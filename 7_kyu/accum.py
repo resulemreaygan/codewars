7e2d765002433fea95e5edc7074cdc00b712001a
Resul Emre AYGAN <remreaygan@gmail.com>
2021-03-15 22:20:27 +0300

    Mumbling added

---
+++ 7_kyu/accum.py
@@ -0,0 +1,20 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+"""
+Project Description: Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
+
+
+def accum(s):
+    return '-'.join([str(s[index] * (index + 1)).capitalize() for index in range(len(s))])
